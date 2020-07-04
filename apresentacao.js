967fecd6b902cc359d71abe5813e75ed7c309825
AirtonHoffmann <airtonbocchese@gmail.com>
2020-07-04 17:37:51 -0300

    mouseInput

--- apresentacao.js
+++ apresentacao.js
@@ -24,46 +24,45 @@ const tabuleiro = {
     },
     casaAltura: 100,
     casaLargura: 100,
-    corCasa: '#00000033',
+    corCasa: '#ffffff00',
     casas: {},
     pecaAltura: 100,
     pecaLargura: 100,
     pecas: {
-        'peaoBranco01': {cor: 'branca', imagem: preloadImagem[1], x: 70, y: 670},
-        'peaoBranco02': {cor: 'branca', imagem: preloadImagem[1], x: 170, y: 670},
-        'peaoBranco03': {cor: 'branca', imagem: preloadImagem[1], x: 270, y: 670},
-        'peaoBranco04': {cor: 'branca', imagem: preloadImagem[1], x: 370, y: 670},
-        'peaoBranco05': {cor: 'branca', imagem: preloadImagem[1], x: 470, y: 670},
-        'peaoBranco06': {cor: 'branca', imagem: preloadImagem[1], x: 570, y: 670},
-        'peaoBranco07': {cor: 'branca', imagem: preloadImagem[1], x: 670, y: 670},
-        'peaoBranco08': {cor: 'branca', imagem: preloadImagem[1], x: 770, y: 670},
-        'torreBranca01': {cor: 'branca', imagem: preloadImagem[2], x: 70, y: 770},
-        'torreBranca02': {cor: 'branca', imagem: preloadImagem[2], x: 770, y: 770},
-        'cavaloBranco01': {cor: 'branca', imagem: preloadImagem[3], x: 170, y: 770},
-        'cavaloBranco02': {cor: 'branca', imagem: preloadImagem[3], x: 670, y: 770},
-        'bispoBranco01': {cor: 'branca', imagem: preloadImagem[4], x: 270, y: 770},
-        'bispoBranco02': {cor: 'branca', imagem: preloadImagem[4], x: 570, y: 770},
-        'rainhaBranca': {cor: 'branca', imagem: preloadImagem[5], x: 370, y: 770},
-        'reiBranc0': {cor: 'branca', imagem: preloadImagem[6], x: 470, y: 770},
+        'peaoBranco01': {imagem: preloadImagem[1], x: 70, y: 670},
+        'peaoBranco02': {imagem: preloadImagem[1], x: 170, y: 670},
+        'peaoBranco03': {imagem: preloadImagem[1], x: 270, y: 670},
+        'peaoBranco04': {imagem: preloadImagem[1], x: 370, y: 670},
+        'peaoBranco05': {imagem: preloadImagem[1], x: 470, y: 670},
+        'peaoBranco06': {imagem: preloadImagem[1], x: 570, y: 670},
+        'peaoBranco07': {imagem: preloadImagem[1], x: 670, y: 670},
+        'peaoBranco08': {imagem: preloadImagem[1], x: 770, y: 670},
+        'torreBranca01': {imagem: preloadImagem[2], x: 70, y: 770},
+        'torreBranca02': {imagem: preloadImagem[2], x: 770, y: 770},
+        'cavaloBranco01': {imagem: preloadImagem[3], x: 170, y: 770},
+        'cavaloBranco02': {imagem: preloadImagem[3], x: 670, y: 770},
+        'bispoBranco01': {imagem: preloadImagem[4], x: 270, y: 770},
+        'bispoBranco02': {imagem: preloadImagem[4], x: 570, y: 770},
+        'rainhaBranca': {imagem: preloadImagem[5], x: 370, y: 770},
+        'reiBranc0': {imagem: preloadImagem[6], x: 470, y: 770},
 
-        'peaoPreto01': {cor: 'preta', imagem: preloadImagem[7], x: 70, y: 170},
-        'peaoPreto02': {cor: 'preta', imagem: preloadImagem[7], x: 170, y: 170},
-        'peaoPreto03': {cor: 'preta', imagem: preloadImagem[7], x: 270, y: 170},
-        'peaoPreto04': {cor: 'preta', imagem: preloadImagem[7], x: 370, y: 170},
-        'peaoPreto05': {cor: 'preta', imagem: preloadImagem[7], x: 470, y: 170},
-        'peaoPreto06': {cor: 'preta', imagem: preloadImagem[7], x: 570, y: 170},
-        'peaoPreto07': {cor: 'preta', imagem: preloadImagem[7], x: 670, y: 170},
-        'peaoPreto08': {cor: 'preta', imagem: preloadImagem[7], x: 770, y: 170},
-        'torrePreta01': {cor: 'preta', imagem: preloadImagem[8], x: 70, y: 70},
-        'torrePreta02': {cor: 'preta', imagem: preloadImagem[8], x: 770, y: 70},
-        'cavaloPreto01': {cor: 'preta', imagem: preloadImagem[9], x: 170, y: 70},
-        'cavaloPreto02': {cor: 'preta', imagem: preloadImagem[9], x: 670, y: 70},
-        'bispoPreto01': {cor: 'preta', imagem: preloadImagem[10], x: 270, y: 70},
-        'bispoPreto02': {cor: 'preta', imagem: preloadImagem[10], x: 570, y: 70},
-        'rainhaPreta': {cor: 'preta', imagem: preloadImagem[11], x: 370, y: 70},
-        'reiPreto': {cor: 'preta', imagem: preloadImagem[12], x: 470, y: 70},
+        'peaoPreto01': {imagem: preloadImagem[7], x: 70, y: 170},
+        'peaoPreto02': {imagem: preloadImagem[7], x: 170, y: 170},
+        'peaoPreto03': {imagem: preloadImagem[7], x: 270, y: 170},
+        'peaoPreto04': {imagem: preloadImagem[7], x: 370, y: 170},
+        'peaoPreto05': {imagem: preloadImagem[7], x: 470, y: 170},
+        'peaoPreto06': {imagem: preloadImagem[7], x: 570, y: 170},
+        'peaoPreto07': {imagem: preloadImagem[7], x: 670, y: 170},
+        'peaoPreto08': {imagem: preloadImagem[7], x: 770, y: 170},
+        'torrePreta01': {imagem: preloadImagem[8], x: 70, y: 70},
+        'torrePreta02': {imagem: preloadImagem[8], x: 770, y: 70},
+        'cavaloPreto01': {imagem: preloadImagem[9], x: 170, y: 70},
+        'cavaloPreto02': {imagem: preloadImagem[9], x: 670, y: 70},
+        'bispoPreto01': {imagem: preloadImagem[10], x: 270, y: 70},
+        'bispoPreto02': {imagem: preloadImagem[10], x: 570, y: 70},
+        'rainhaPreta': {imagem: preloadImagem[11], x: 370, y: 70},
+        'reiPreto': {imagem: preloadImagem[12], x: 470, y: 70},
     },
-    ultimaPeca: null
 }
 
 pX = 70
@@ -91,17 +90,16 @@ function renderScreen() {
 
     for(const casaId in tabuleiro.casas){
         const casa = tabuleiro.casas[casaId]
-        context.fillStyle = casa.color
-        context.fillRect(casa.x, casa.y, tabuleiro.casaLargura, tabuleiro.casaAltura)
+
+        context.strokeStyle = casa.color
+        context.lineWidth = "2"
+        context.strokeRect(casa.x, casa.y, tabuleiro.casaLargura, tabuleiro.casaAltura)
     }
 
     for(const pecaId in tabuleiro.pecas){
         const peca = tabuleiro.pecas[pecaId]
         context.drawImage(peca.imagem, peca.x, peca.y)
     }
-
-    if(tabuleiro.ultimaPeca != null)
-        context.drawImage(tabuleiro.pecas[tabuleiro.ultimaPeca].imagem, tabuleiro.pecas[tabuleiro.ultimaPeca].x, tabuleiro.pecas[tabuleiro.ultimaPeca].y)
 
     requestAnimationFrame(renderScreen)
 }
